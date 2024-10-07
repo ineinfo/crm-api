f4558bcb379a36aeaad0f994d829482d678ccd19
Nilesh Ratan Gupta <ineinfotechservices@gmail.com>
2024-10-07 16:13:31 +0530

    change

--- src/routes/salesProgressionPortal.js
+++ src/routes/salesProgressionPortal.js
@@ -6,8 +6,21 @@ const bcrypt = require('bcryptjs');
 const pool = require('../utils/db');
 const router = express.Router();
 const authenticateToken = require('../utils/middleware');
+const multer = require('multer');
 
 let moduleTitle = 'Sales Progression';
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'public/lead_documents/'); 
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+      cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({ storage: storage });
 
 // Get an offer list
 router.get('/status',authenticateToken, async (req, res) => {
@@ -64,9 +77,9 @@ router.get('/:lead_id?',authenticateToken, async (req, res) => {
 });
 
 
-router.put('/updatestatus',authenticateToken, async (req, res) => {
+router.put('/updatestatus',upload.fields([{ name: 'document' }]), authenticateToken, async (req, res) => {
     const user_id = req.user.id
-    const { lead_id, amount, lead_status } = req.body;
+    const { lead_id, amount, lead_status, document } = req.body;
     if (!lead_id || !lead_status) {
         return res.status(400).json({ message: 'Please provide all fields', status: 'error' });
     }
@@ -84,6 +97,21 @@ router.put('/updatestatus',authenticateToken, async (req, res) => {
         let result = null;
         if(lead_status == 1) {
             [result] = await pool.query(`INSERT INTO ${TABLE.LEAD_SALES_STATUS_LIST_TABLE} (lead_id, user_id, amount, lead_status) VALUES (?, ?, ?, ?)`, [lead_id, user_id, amount, lead_status]);
+        }
+
+        if(lead_status == 2) {
+            [result] = await pool.query(`INSERT INTO ${TABLE.LEAD_SALES_STATUS_LIST_TABLE} (lead_id, user_id, lead_status) VALUES (?, ?, ?)`, [lead_id, user_id, lead_status]);
+        }
+
+        if(lead_status == 3) {
+            let documentPath = null;
+
+            const documentUrl = req.files['document'] 
+            ? `${req.protocol}://${req.get('host')}/lead_documents/${req.files['document'][0].filename}` 
+            : null;
+        
+
+            [result] = await pool.query(`INSERT INTO ${TABLE.LEAD_SALES_STATUS_LIST_TABLE} (lead_id, user_id, lead_status, document_url) VALUES (?, ?, ?, ?)`, [lead_id, user_id, lead_status, documentUrl]);
         }
 
         await pool.query(`UPDATE ${TABLE.LEADS_TABLE} SET lead_status = ?, lead_sales_status_list_id = ? WHERE id = ?`, [lead_status, result.insertId, lead_id]);
