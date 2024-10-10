825474ed59a954c38ce25cc749f1557f10c6f3a0
Nilesh Ratan Gupta <ineinfotechservices@gmail.com>
2024-10-10 12:47:09 +0530

    change

--- src/routes/salesProgressionPortal.js
+++ src/routes/salesProgressionPortal.js
@@ -245,7 +245,7 @@ router.put('/updatestatus',upload.fields([{ name: 'document' }]), authenticateTo
             ? `${req.protocol}://${req.get('host')}/lead_complete_documents/${req.files['document'][0].filename}` 
             : null;
             
-            [result] = await pool.query(`INSERT INTO ${TABLE.LEAD_SALES_STATUS_LIST_TABLE} (lead_id, user_id, lead_status, completion_date, complete_document_url) VALUES (?, ?, ?, ?)`, [lead_id, user_id, lead_status, completion_date_db, completeDocumentUrl]);
+            [result] = await pool.query(`INSERT INTO ${TABLE.LEAD_SALES_STATUS_LIST_TABLE} (lead_id, user_id, lead_status, completion_date, complete_document_url) VALUES (?, ?, ?, ?, ?)`, [lead_id, user_id, lead_status, completion_date_db, completeDocumentUrl]);
 
             await pool.query(`UPDATE ${TABLE.LEADS_TABLE} SET status=3  WHERE id = ?`, [lead_id]);
 
